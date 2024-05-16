6a8c23e1a0381631eb9375e7c8c850fe238be4b0
nrsk2k4 <neelamramachandransujithkumar@gmail.com>
2024-05-16 13:21:57 +0530

    renamed folder names to follow order

--- chap1_variables_and_datatypes/varibles.java
+++ chap1_variables_and_datatypes/varibles.java
@@ -1,5 +1,5 @@
 // A varible is a container that stores a value.This value can be changed during the execution of th program
-package variables_and_datatypes_1;
+package chap1_variables_and_datatypes;
 
 class varibles {
 
