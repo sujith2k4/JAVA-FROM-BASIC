88c12499d494a3521cad33db19b77d9584de746b
nrsk2k4 <neelamramachandransujithkumar@gmail.com>
2024-05-16 13:08:30 +0530

    examples of variable declaration in java

--- variables_and_datatypes_1/varibles.java
+++ variables_and_datatypes_1/varibles.java
@@ -1,1 +1,10 @@
+// A varible is a container that stores a value.This value can be changed during the execution of th program
+package variables_and_datatypes_1;
 
+class varibles {
+
+    public static void main(String[] args) {
+        int number=4;
+        System.out.println(number*4);
+    }
+}
