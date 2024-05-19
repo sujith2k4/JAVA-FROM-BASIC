47117f91a732143f5052866e4767a1e221a2dc69
nrsk2k4 <neelamramachandransujithkumar@gmail.com>
2024-05-19 20:08:11 +0530

    an example for taking input form the user

--- chap1_variables_and_datatypes/varibles.java
+++ chap1_variables_and_datatypes/varibles.java
@@ -1,10 +1,18 @@
 // A varible is a container that stores a value.This value can be changed during the execution of th program
 package chap1_variables_and_datatypes;
+import java.util.Scanner;
 
 class varibles {
 
     public static void main(String[] args) {
-        int number=4;
+        Scanner ns =new Scanner(System.in);
+        System.out.println("enter number 1:");
+        int num1=ns.nextInt();
+        System.out.println("enter number 2");
+        int num2=ns.nextInt();
+        int number=num1+num2;
+        
+       
         System.out.println(number*4);
     }
 }
