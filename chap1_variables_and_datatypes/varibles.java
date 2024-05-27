63d90504e20b01fd37f135746acbe0472689d083
sujith2k4 <23102a040314@mbu.asia>
2024-05-27 13:49:30 +0530

    exmpled changed

--- chap1_variables_and_datatypes/varibles.java
+++ chap1_variables_and_datatypes/varibles.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 class varibles {
 
     public static void main(String[] args) {
-        Scanner ns =new Scanner(System.in);
-        System.out.println("enter number 1:");
-        int num1=ns.nextInt();
-        System.out.println("enter number 2");
-        int num2=ns.nextInt();
-        int number=num1+num2;
-        
-       
-        System.out.println(number*4);
+        try (Scanner ns = new Scanner(System.in)) {
+            System.out.println("enter number 1:");
+            int num1 = ns.nextInt();
+            System.out.println("enter number 2");
+            int num2 = ns.nextInt();
+            int number = num1 + num2;
+
+            System.out.println(number * 4);
+        }
     }
 }
