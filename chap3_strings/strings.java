98877bd3aeba7af8c4b518b6d52708bff50ca6c2
sujith2k4 <23102a040314@mbu.asia>
2024-05-27 12:03:55 +0530

    examples of string

--- chap3_strings/strings.java
+++ chap3_strings/strings.java
@@ -1,1 +1,52 @@
+package chap3_strings;
 
+class strings {
+
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(6));
+        System.out.println(str.charAt(11));
+        System.out.println(str.charAt(12));
+        System.out.println(str.substring(0, 5));
+        System.out.println(str.substring(6, 11));
+        System.out.println(str.substring(6));
+        System.out.println(str.substring(6, 6));
+        System.out.println(str.substring(6, 5));
+        System.out.println(str.substring(6, 12));
+        System.out.println(str.substring(6, 13));
+        System.out.println(str.substring(6, 14));
+        System.out.println(str.substring(6, 15));
+        System.out.println(str.substring(6, 16));
+        System.out.println(str.substring(6, 17));
+        System.out.println(str.substring(6, 18));
+        System.out.println(str.substring(6, 19));
+        System.out.println(str.substring(6, 20));
+        System.out.println(str.substring(6, 21));
+        System.out.println(str.substring(6, 22));
+        System.out.println(str.substring(6, 23));
+        System.out.println(str.substring(6, 24));
+        System.out.println(str.substring(6, 25));
+        System.out.println(str.substring(6, 26));
+        System.out.println(str.substring(6, 27));
+        System.out.println(str.substring(6, 28));
+        System.out.println(str.substring(6, 29));
+        System.out.println(str.substring(6, 30));
+        System.out.println(str.substring(6, 31));
+        System.out.println(str.substring(6, 32));
+        System.out.println(str.substring(6, 33));
+        System.out.println(str.substring(6, 34));
+        System.out.println(str.substring(6, 35));
+        System.out.println(str.substring(6, 36));
+        System.out.println(str.substring(6, 37));
+        System.out.println(str.substring(6, 38));
+        System.out.println(str.substring(6, 39));
+        System.out.println(str.substring(6, 40));
+        System.out.println(str.substring(6, 41));
+        System.out.println(str.substring(6, 42));
+        System.out.println(str.substring(6, 43));
+        
+    }
+}
